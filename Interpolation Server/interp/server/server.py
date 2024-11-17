bb7756df655089152aa88eaa29071939b8b6a216
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2024-11-16 21:46:26 -0500

    Byte stream test

--- Interpolation Server/interp/server/server.py
+++ Interpolation Server/interp/server/server.py
@@ -3,7 +3,7 @@ import interp.nn.model as model
 import torch
 
 from fastapi import FastAPI
-from fastapi.routing import APIRouter
+from fastapi.routing import APIRouter, Response
 
 class InterpolationServerView:
     def __init__(self, output_dir:str, nn_params_file:str):
@@ -11,15 +11,19 @@ class InterpolationServerView:
         self.router.add_api_route("/test", self.test_removethismethod, methods=["GET"])
         
         self.output_dir = output_dir
-        self.model = model.Interpolator()
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = self.model.to(device)
-        self.model.load_state_dict(torch.load(nn_params_file, weights_only=True))
-        self.model.eval()
+        #self.model = model.Interpolator()
+        #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        #self.model = self.model.to(device)
+        #self.model.load_state_dict(torch.load(nn_params_file, weights_only=True))
+        #self.model.eval()
         pass
     
-    def test_removethismethod(self) -> dict:
+    def test_old(self) -> dict:
         return {"message": "Hello world"}
+    
+    def test_removethismethod(self) -> Response:
+        content = b'Hello world'
+        return Response(content=content, media_type='application/octet-stream')
     
     def interpolate_and_save(self, input_video:str, output_video:str):
         pass
