1790a0220b09bbb4d515c31d5e8861542643a103
Tomas Ambrosini <tomasambrosini28@gmail.com>
2024-11-19 22:23:02 -0500

    UI Prototype

--- Interpolation Client/app/src/main/java/interpolation/client/video/VideoTransfer.java
+++ Interpolation Client/app/src/main/java/interpolation/client/video/VideoTransfer.java
@@ -11,16 +11,33 @@ public class VideoTransfer {
 
     // Returns the filename that will be downloaded once processing is done
     public String uploadVideo(File video) {
+        if (domain != null) {
+            return domain + "/upload/" + video.getName(); // Simulated URL for uploaded video
+        }
         return null;
     }
 
     // True if serverVideo exists on server and download successful, False otherwise
     public boolean downloadVideo(String serverVideo, File output) {
+        if (domain != null) {
+            System.out.println("Downloading: " + domain + "/videos/" + serverVideo);
+            return true; // Simulate successful download
+        }
         return false;
     }
 
     // True if serverVideo exists on server
     public boolean videoExists(String serverVideo) {
+        if (domain != null) {
+            System.out.println("Checking existence of: " + domain + "/videos/" + serverVideo);
+            return true; // Simulate video existence
+        }
         return false;
     }
+
+    // Getter for domain
+    public String getDomain() {
+        return domain;
+    }
 }
+
