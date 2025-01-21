d11b53bf5c730d8b123284aca6bf9313487365ec
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2025-01-21 11:00:42 -0500

    Restructure project

---
+++ nnqueue/src/main/java/net/kpuig/nnqueue/NnqueueApplication.java
@@ -0,0 +1,13 @@
+package net.kpuig.nnqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnqueueApplication.class, args);
+	}
+
+}
---
+++ nnqueue/src/test/java/net/kpuig/nnqueue/NnqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kpuig.nnqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
