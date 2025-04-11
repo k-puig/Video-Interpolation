97c65437bb1d248aedd721c650ea74f10a402d82
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2025-04-11 16:35:08 +0000

    Update status page, added top bar, added contact page

--- interp-website/src/App.tsx
+++ interp-website/src/App.tsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import UploadPage from './pages/UploadPage';
 import StatusPage from './pages/StatusPage';
+import TopBar from './pages/components/TopBar'
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<UploadPage />} />
-      <Route path="/status" element={<StatusPage />} />
-    </Routes>
+    <>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<UploadPage />} />
+        <Route path="/status" element={<StatusPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </>
   );
 }
 
--- interp-website/src/pages/StatusPage.tsx
+++ interp-website/src/pages/StatusPage.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 function StatusPage() {
+  const [inputFileName, setInputFileName] = useState('');
   const [searchParams] = useSearchParams();
-  const fileParam = searchParams.get('file');
+  const [fileParam, setFileParam] = useState(searchParams.get('file'));
   const [status, setStatus] = useState('LOADING');
   const [error, setError] = useState('');
 
@@ -56,14 +57,43 @@ function StatusPage() {
     }
   };
 
+  const handleInputFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      e.target.value = e.target.value.replace("\n", "");
+      setInputFileName(e.target.value);
+    }
+  };
+
+  const handleSwitchStatus = () => {
+    setFileParam(inputFileName);
+    setError('')
+  };
+  
+
   return (
     <div className="container">
       <h1>Video File Status</h1>
-      {error && <p style={{ color: '#ff6666' }}>{error}</p>}
+      {error && (
+        <>
+          <p style={{ color: '#ff6666' }}>{error}</p>
+          <div style={{ marginTop:"1rem" }}>
+            <label htmlFor="file">Enter the video filename you want to check the status of:</label>
+            <input id="inputFileName" type="text" onChange={handleInputFileChange} placeholder="Enter filename..."></input>
+            <div style={{ marginTop: '1rem' }}>
+              <button onClick={handleSwitchStatus}>View File Status</button>
+            </div>
+          </div>
+        </>
+      )}
       {!error && (
         <>
           <p>
             Feel free to save the URL and/or filename to check the status again at any time!
+          </p>
+          <p>
+            If you feel like your video is taking especially long to process, it's likely that
+            either your video has a large number of frames, the neural network processor is in the
+            middle of training, or the neural network processor is down altogether.
           </p>
           <p>
             Checking status for:
